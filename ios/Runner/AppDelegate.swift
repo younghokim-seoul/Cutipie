3827991f7a1edc2a8a074039712613d5c3adfff6
sycheon88 <sycheon88@gmail.com>
2024-11-14 15:00:11 +0900

    1.0.0(2)
    FCM 권한 추가
    Record 권한 추가
    인앱결제 capability 추가
    p8 인증서 파일 추가

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,20 @@
 import Flutter
 import UIKit
+import FirebaseMessaging
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        GeneratedPluginRegistrant.register(with: self)
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
+
+    override func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+
+       Messaging.messaging().apnsToken = deviceToken
+       super.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
+     }
 }
