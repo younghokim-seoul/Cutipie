645e7d6845a55e7c0e2f3f93a60d9ca59c0b638c
sycheon88 <sycheon88@gmail.com>
2024-11-15 15:00:18 +0900

    flutter_downloader 적용

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,8 @@
 import Flutter
 import UIKit
 import FirebaseMessaging
+import flutter_downloader
+
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -9,12 +11,23 @@ import FirebaseMessaging
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
         GeneratedPluginRegistrant.register(with: self)
+        //flutter_downloader settings
+        FlutterDownloaderPlugin.setPluginRegistrantCallback(registerPlugins)
         return super.application(application, didFinishLaunchingWithOptions: launchOptions)
     }
 
+    //FirebaseMessaging settings
     override func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
 
-       Messaging.messaging().apnsToken = deviceToken
-       super.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
-     }
+        Messaging.messaging().apnsToken = deviceToken
+        super.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
+    }
+
+}
+
+//flutter_downloader settings
+private func registerPlugins(registry: FlutterPluginRegistry) {
+    if (!registry.hasPlugin("FlutterDownloaderPlugin")) {
+        FlutterDownloaderPlugin.register(with: registry.registrar(forPlugin: "FlutterDownloaderPlugin")!)
+    }
 }
