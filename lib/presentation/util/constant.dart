c939ab724a17d766eaaa59f99dc4bc3a25fe97ef
sycheon <sycheon88@gmail.com>
2025-01-15 17:04:07 +0900

    iOS 1.0.3(14)

--- lib/presentation/util/constant.dart
+++ lib/presentation/util/constant.dart
@@ -7,7 +7,7 @@ import 'package:cutipie/presentation/util/dev_log.dart';
 class Const {
   static LogLevel logLevel = LogLevel.v;
   static bool useProxy = false;
-  static bool isDevMode = true;
+  static bool isDevMode = false;
   static String proxyAddress = "192.168.206.119:8888";
 
 }
